a3994ac0f206178be268867654de71e5f7fe5424
chenjiecqupt <904281072@qq.com>
2018-04-13 17:33:18 +0800

    完善Mock数据

--- src/Mock.js
+++ src/Mock.js
@@ -9,7 +9,27 @@ require.config({
 const Mock = require('mockjs')
 //使用mockjs模拟数据
 Mock.mock('/api/data', (req, res) => {
-  return {
-    data: ['a','b','qweqw','qwe']
-  }
+  return Mock.mock({
+    "array|10": [
+      {
+        'name': '@cname',
+        /*'age|18-80':100,*/
+        "phoneNumber_1":"1",
+        "phoneNumber_2|+1":[31,32,33,34,35,36,37,38,39,47,50,82,83,87,88,89],
+       /* "phoneNumber_3|10000000-99999999":"1",*/
+       /* "identityCard_1|100000-999999":"1",
+        "identityCard_2|1000-9999":"1",*/
+        'birthday':'@date("yyyy-MM-dd")',
+        'city':'@city(true)'
+      }
+    ]
+  })
+  /*return {
+    "list|1-10":[{
+    'name': '@cname',
+    'age|18-80':100,
+    'birthday':'@date("yyyy-MM-dd")',
+    'city':'@city(true)'
+  }]
+  }*/
 })
--- src/components/MockTest.vue
+++ src/components/MockTest.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="hello">
-        <h1>{{ data }}</h1>
+      <h1>下列数据由Mock和随机函数生成，并非真实数据</h1>
+      <div>
+        <table class="cpt-table">
+          <thead class="cpt-thead">
+            <th class="cpt-th" width="12%">姓名</th>
+            <th class="cpt-th" width="12%">年龄</th>
+            <th class="cpt-th" width="12%">手机</th>
+            <th class="cpt-th" width="12%">出生年月日</th>
+            <th class="cpt-th" width="26%">城市</th>
+            <th class="cpt-th" width="26%">证件号码</th>
+          </thead>
+          <tbody class="cpt-tbody">
+            <tr class="cpt-tr" v-for="person in data">
+              <td class="cpt-td">{{person.name}}</td>
+              <td class="cpt-td">{{person.age}}</td>
+              <td class="cpt-td">{{person.phoneNumber}}</td>
+              <td class="cpt-td">{{person.birthday}}</td>
+              <td class="cpt-td">{{person.city}}</td>
+              <td class="cpt-td">{{person.identityCard}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <h1>Mock数据调用：{{code}}</h1>
     </div>
 </template>
 
@@ -9,14 +32,56 @@
       name: 'hello',
       data() {
           return {
-              data:[]
+              data:[],
+              code:'this.$axios.get("/api/data").then(res=>{console.log(res)}',
+              thisYear:new Date().getFullYear()
           }
       },
-      mounted: function () {
+      methods:{
+        randomNumber:(n)=>{
+          if(!Number.isInteger(n) && n==0){
+            console.error('randomNumber(n),n请输入大于0的整数!');
+            return;
+          }
+          let num = '';
+          for(var i=0;i<n;i++){
+            let str = null;
+            str = Math.floor(Math.random()*10);
+            if(i == 0&&str==0){
+              str =1
+            }
+            num += str;
+          }
+          return num;
+        }
+      },
+      beforeCreate:function () {
+        let _this = this
         this.$axios.get('/api/data').then(res=>{
-          this.data = res.data.data;
-          console.log(res);
+          for(let index in res.data.array){
+            res.data.array[index].age =  _this.thisYear - res.data.array[index].birthday.split('-')[0];
+            res.data.array[index].phoneNumber = res.data.array[index].phoneNumber_1+res.data.array[index].phoneNumber_2 + _this.randomNumber(8);
+            res.data.array[index].identityCard = _this.randomNumber(6)+ res.data.array[index].birthday.split('-').join('') + _this.randomNumber(4);
+          }
+          console.log(res.data.array);
+          this.data = res.data.array;
         })
+      },
+      created: function () {
+        let _this = this;
+        this.timer = setInterval(function () {
+          _this.$axios.get('/api/data').then(res=>{
+            for(let index in res.data.array){
+              res.data.array[index].age =  _this.thisYear - res.data.array[index].birthday.split('-')[0];
+              res.data.array[index].phoneNumber = res.data.array[index].phoneNumber_1 + res.data.array[index].phoneNumber_2 + _this.randomNumber(8);
+              res.data.array[index].identityCard = _this.randomNumber(6) + res.data.array[index].birthday.split('-').join('') + _this.randomNumber(4);
+            }
+            _this.data = res.data.array;
+          })
+        },6000);
+      },
+      beforeDestroy:function () {
+        if(this.timer)clearInterval(this.timer);
       }
     }
 </script>
@@ -25,6 +90,13 @@
 <style scoped>
     h1, h2 {
         font-weight: normal;
+    }
+    tr:nth-of-type(odd){
+      background: #f5f9fc;
+      color: #000002;
+    }
+    tr:hover{
+      color: #1abc9c;
     }
 </style>
 
